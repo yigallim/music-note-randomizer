5a87340f77e9f32fce5875c1c983455ce9976d3c
Ye Yang 业阳 <111426810+yigallim@users.noreply.github.com>
2024-08-28 00:40:51 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app.tsx";
 import "@fontsource-variable/inter";
 import "./global.css";
 import "./index.css";
